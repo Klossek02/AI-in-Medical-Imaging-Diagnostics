93740aba842d954fe19ede68c423319fdb07cc7a
mytkom <marek.mytkowski.mm@gmail.com>
2026-01-15 22:57:40 +0100

    finaly fix poly scheduler

--- src/scheduler.py
+++ src/scheduler.py
@@ -21,6 +21,7 @@ def get_scheduler(optimizer, config: Config):
     elif config.training.scheduler_type == "PolyLR":
         return optim.lr_scheduler.PolynomialLR(
             optimizer,
+            total_iters=config.training.num_epochs,
             **config.training.scheduler_params
         )
     elif config.training.scheduler_type == "CosineAnnealingLR":
